8ccb7d6605d9ff3ceaf4e62ae4e7ad16ffbc67d4
ADITYA <agorrapati7@gmail.com>
2015-08-24 19:54:52 -0500

    Updated Version
    Updated Version on August 24th 2015

--- app/src/main/java/aditya/com/googlemaps/MapsActivity.java
+++ app/src/main/java/aditya/com/googlemaps/MapsActivity.java
@@ -1,21 +1,34 @@
 package aditya.com.googlemaps;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    Button search;// Might be null if Google Play services APK is not available.
+    private GoogleMap gMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        search = (Button) findViewById(R.id.LSearch);
         setUpMapIfNeeded();
     }
 
@@ -25,41 +38,76 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
+        if (gMaps == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            gMaps = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
+            if (gMaps != null) {
+
             }
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+    private void setUpMap(LatLng latLng) {
+        gMaps.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+        gMaps.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                View view = getLayoutInflater().inflate(R.layout.marker, null);
+                LatLng latLng1 = marker.getPosition();
+                TextView tvLat = (TextView) view.findViewById(R.id.markerlat);
+                TextView tvLng = (TextView) view.findViewById(R.id.markerlng);
+                tvLat.setText("Latitude: " + latLng1.latitude);
+                tvLng.setText("Longitude: " + latLng1.longitude);
+
+
+                return view;
+            }
+        });
+
+        gMaps.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                gMaps.clear();
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                gMaps.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                Marker marker = gMaps.addMarker(markerOptions);
+                marker.showInfoWindow();
+            }
+        });
+        gMaps.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    public void onSearch(View view) {
+
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.actv);
+        String location = autoCompleteTextView.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+            setUpMap(latlng);
+            /*
+            gMaps.addMarker(new MarkerOptions().position(latlng).title("Marker"));
+            gMaps.animateCamera(CameraUpdateFactory.newLatLng(latlng));*/
+        }
+
     }
 }
